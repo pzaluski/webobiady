facef086ec0e76fb66734bed80638d6299ebba48
mzaluska <martyna.borkowicz@gmail.com>
2017-06-04 17:44:28 +0200

    Rejestracja / Aktywacja konta / maile

--- user/views.py
+++ user/views.py
@@ -1,67 +1,12 @@
 from datetime import date
 
-from django.contrib.auth import authenticate, login, update_session_auth_hash
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import PasswordChangeForm
-from django.shortcuts import render, redirect
-from django.views.generic import View
+from django.shortcuts import render
 
 from orders.models import Order
-from .forms import UserRegisterForm
 
 
 @login_required
 def home(request):
     uo = Order.objects.orders_for_user(user=request.user, order_date=date.today())
     return render(request, "user/home.html", {'uo': uo})
-
-@login_required
-def change_password(request):
-    if request.method == 'POST':
-        form = PasswordChangeForm(data=request.POST, user=request.user)
-
-        if form.is_valid():
-            form.save()
-            update_session_auth_hash(request, form.user)
-            return redirect('user_home')
-        else:
-            return redirect('change_password')
-    else:
-        form = PasswordChangeForm(user=request.user)
-        args = {'form': form}
-        return render(request, 'user/change_password.html', args)
-
-
-class UserRegisterView(View):
-    form_class = UserRegisterForm
-    template_name = 'user/registration.html'
-
-    def get(self, request):
-        form = self.form_class(None)
-        return render(request, self.template_name, {'form': form})
-
-    def post(self, request):
-        form = self.form_class(request.POST)
-
-        if form.is_valid():
-            user = form.save(commit=False)
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            email = form.cleaned_data['email']
-            first_name = form.cleaned_data['first_name']
-            user.set_password(password)
-            user.save()
-
-            user = authenticate(username=username, password=password)
-
-            if user is not None:
-
-                if user.is_active:
-
-                    login(request, user)
-                    return redirect('user_home')
-
-        return render(request, self.template_name, {'form': form})
-
-
-
