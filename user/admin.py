4fef3f3114a3d842e540739c1a0185881c2601f7
mzaluska <martyna.borkowicz@gmail.com>
2017-06-10 16:08:09 +0200

    Zamówienia - edycja

---
+++ user/admin.py
@@ -0,0 +1,11 @@
+'''
+from .models import UserProfile
+from django.contrib import admin
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
+'''
---
+++ user/models.py
@@ -0,0 +1,20 @@
+'''
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    is_purchaser = models.BooleanField(default=False, verbose_name='Kupujący')
+
+    def __str__(self):
+        return '{0} - {1}'.format(self.user.username, self.is_purchaser)
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        profile, new = UserProfile.objects.get_or_create(user=instance)
+'''
