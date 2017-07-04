b15ffb4bc8b77d0bd7ba71e605836ada09a2b67b
mzaluska <martyna.borkowicz@gmail.com>
2017-07-04 21:56:40 +0200

    Poprawka urla do restauracji

--- restaurants/models.py
+++ restaurants/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Restaurant(models.Model):
     name = models.CharField(max_length=50)
-    menu_url = models.CharField(max_length=50)
+    menu_url = models.CharField(max_length=400)
     delivery_price = models.DecimalField(max_digits=5, decimal_places=2)
 
     class Meta:
