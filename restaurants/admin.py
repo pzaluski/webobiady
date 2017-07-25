b68f46426a92bd96826c7465c1dd1aaa74ac9f35
mzaluska <martyna.borkowicz@gmail.com>
2017-07-25 18:50:15 +0200

    Admin

--- restaurants/admin.py
+++ restaurants/admin.py
@@ -2,6 +2,14 @@ from django.contrib import admin
 
 from .models import Restaurant, Category, Dish
 
+
+class DishModelAdmin(admin.ModelAdmin):
+    list_display = ["name", "price", "restaurant"]
+    list_filter = ["restaurant"]
+
+    class Meta:
+        model = Dish
+
 admin.site.register(Restaurant)
 admin.site.register(Category)
-admin.site.register(Dish)
+admin.site.register(Dish, DishModelAdmin)
